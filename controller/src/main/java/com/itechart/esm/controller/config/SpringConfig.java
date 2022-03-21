702dc834c47a0796bfd90fd99a1b2de99dcde72d
Mikhail Leonovets <mikhail.leonovets@gmail.com>
2022-03-21 18:44:36 +0300

    app config updated

--- controller/src/main/java/com/itechart/esm/controller/config/SpringConfig.java
+++ controller/src/main/java/com/itechart/esm/controller/config/SpringConfig.java
@@ -4,8 +4,12 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.converter.HttpMessageConverter;
+import org.springframework.http.converter.json.MappingJackson2HttpMessageConverter;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+import java.util.List;
 
 @Configuration
 @EnableWebMvc
@@ -16,5 +20,10 @@ public class SpringConfig implements WebMvcConfigurer {
 	@Autowired
 	public SpringConfig(ApplicationContext applicationContext) {
 		this.applicationContext = applicationContext;
+	}
+
+	@Override
+	public void configureMessageConverters(List<HttpMessageConverter<?>> converters) {
+		converters.add(new MappingJackson2HttpMessageConverter());
 	}
 }
