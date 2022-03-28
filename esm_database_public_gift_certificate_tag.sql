50f94b34b1eb0307811f8635ad0c9900f8e61008
Mikhail Leonovets <mikhail.leonovets@gmail.com>
2022-03-28 16:33:02 +0300

    sql scripts added

---
+++ esm_database_public_gift_certificate_tag.sql
@@ -0,0 +1,10 @@
+INSERT INTO public.gift_certificate_tag (id, gift_certificate_id, tag_id) VALUES (2, 4, 1);
+INSERT INTO public.gift_certificate_tag (id, gift_certificate_id, tag_id) VALUES (3, 4, 2);
+INSERT INTO public.gift_certificate_tag (id, gift_certificate_id, tag_id) VALUES (4, 4, 3);
+INSERT INTO public.gift_certificate_tag (id, gift_certificate_id, tag_id) VALUES (5, 5, 2);
+INSERT INTO public.gift_certificate_tag (id, gift_certificate_id, tag_id) VALUES (6, 6, 2);
+INSERT INTO public.gift_certificate_tag (id, gift_certificate_id, tag_id) VALUES (7, 6, 1);
+INSERT INTO public.gift_certificate_tag (id, gift_certificate_id, tag_id) VALUES (8, 5, 3);
+INSERT INTO public.gift_certificate_tag (id, gift_certificate_id, tag_id) VALUES (13, 9, 1);
+INSERT INTO public.gift_certificate_tag (id, gift_certificate_id, tag_id) VALUES (16, 14, 1);
+INSERT INTO public.gift_certificate_tag (id, gift_certificate_id, tag_id) VALUES (19, 16, 4);
---
+++ esm_database_public_tag.sql
@@ -0,0 +1,6 @@
+INSERT INTO public.tag (id, name) VALUES (1, 'Tag1');
+INSERT INTO public.tag (id, name) VALUES (2, 'Tag2');
+INSERT INTO public.tag (id, name) VALUES (3, 'Tag3');
+INSERT INTO public.tag (id, name) VALUES (4, 'Tag4');
+INSERT INTO public.tag (id, name) VALUES (5, 'Tag10');
+INSERT INTO public.tag (id, name) VALUES (6, 'Tag11');
