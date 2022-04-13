244a591e5d5cbc30bb71878444e673152b92177d
Mikhail Leonovets <mikhail.leonovets@gmail.com>
2022-04-13 13:37:10 +0300

    Javadoc in GiftCertificateTagService created Javadoc in GiftCertificateManagementService created

--- service/src/main/java/com/itechart/esm/service/GiftCertificateManagementService.java
+++ service/src/main/java/com/itechart/esm/service/GiftCertificateManagementService.java
@@ -9,43 +9,137 @@ import com.itechart.esm.service.model.GiftCertificateAndItsTags;
 
 import java.util.List;
 
+/**
+ * This class is for combining Tags and GiftCertificates into GiftCertificateAndItsTags which present to the client
+ * all information of the GiftCertificate
+ */
 public interface GiftCertificateManagementService {
 
+	/**
+	 * @param giftCertificateAndItsTags creates this giftCertificateAndItsTags and saves it in the database
+	 * @throws DataInputException when giftCertificateAndItsTags is null
+	 */
 	void createGiftCertificate(GiftCertificateAndItsTags giftCertificateAndItsTags) throws DataInputException;
 
+	/**
+	 * @return lift of all GiftCertificateAndItsTags stored in the database
+	 * @throws DataInputException               when something goes wrong with data in the database
+	 * @throws TagNotFoundException             when something goes wrong with data in the database
+	 * @throws GiftCertificateNotFoundException when something goes wrong with data in the database
+	 */
 	List<GiftCertificateAndItsTags> findAll() throws DataInputException, TagNotFoundException,
 			GiftCertificateNotFoundException;
 
+	/**
+	 * @return list of all GiftCertificateAndItsTags stored in the database sorted by GiftCertificate field
+	 * DateOfCreation
+	 * @throws TagNotFoundException             when something goes wrong with data in the database
+	 * @throws GiftCertificateNotFoundException when something goes wrong with data in the database
+	 * @throws DataInputException               when something goes wrong with data in the database
+	 */
 	List<GiftCertificateAndItsTags> findAllSortByDateOfCreation()
 			throws TagNotFoundException, GiftCertificateNotFoundException, DataInputException;
 
+	/**
+	 * @return list of all GiftCertificateAndItsTags stored in the database sorted by GiftCertificate field name
+	 * @throws TagNotFoundException             when something goes wrong with data in the database
+	 * @throws GiftCertificateNotFoundException when something goes wrong with data in the database
+	 * @throws DataInputException               when something goes wrong with data in the database
+	 */
 	List<GiftCertificateAndItsTags> findAllSortByName()
 			throws TagNotFoundException, GiftCertificateNotFoundException, DataInputException;
 
+	/**
+	 * @param id of GiftCertificate is for searching GiftCertificateAndItsTags
+	 * @return GiftCertificateAndItsTags which contains GiftCertificate with this id
+	 * @throws GiftCertificateNotFoundException when something goes wrong with data in the database
+	 * @throws DataInputException               when GiftCertificate with this id is not found
+	 * @throws TagNotFoundException             when something goes wrong with data in the database
+	 */
 	GiftCertificateAndItsTags findByGiftCertificateId(Long id) throws GiftCertificateNotFoundException, DataInputException,
 			TagNotFoundException;
 
+	/**
+	 * @param tag is for searching all GiftCertificateAndItsTags with this it
+	 * @return list of GiftCertificateAndItsTags which stored in the database with this tag
+	 * @throws DataInputException               when something goes wrong with data in the database or tag is null
+	 * @throws TagNotFoundException             when this tag or any adjacent tag is not found in the database
+	 * @throws GiftCertificateNotFoundException when something goes wrong with data in the database
+	 */
 	List<GiftCertificateAndItsTags> findByTag(Tag tag) throws DataInputException, TagNotFoundException,
 			GiftCertificateNotFoundException;
 
+	/**
+	 * @param tagName is for searching all GiftCertificateAndItsTags with tag with this name
+	 * @return list of GiftCertificateAndItsTags which stored in the database with this tag which is with this name
+	 * @throws DataInputException               when something goes wrong with data in the database or tagName is null
+	 * @throws TagNotFoundException             when something goes wrong with data in the database
+	 * @throws GiftCertificateNotFoundException when something goes wrong with data in the database
+	 */
 	List<GiftCertificateAndItsTags> findByTag(String tagName) throws DataInputException, TagNotFoundException,
 			GiftCertificateNotFoundException;
 
+	/**
+	 * @param tagId is for searching all GiftCertificateAndItsTags with tag with this id
+	 * @return list of GiftCertificateAndItsTags which stored in the database with this tag which is with this id
+	 * @throws DataInputException               when something goes wrong with data in the database or tagId is null
+	 * @throws TagNotFoundException             when something goes wrong with data in the database
+	 * @throws GiftCertificateNotFoundException when something goes wrong with data in the database
+	 */
 	List<GiftCertificateAndItsTags> findByTag(Long tagId) throws DataInputException, TagNotFoundException,
 			GiftCertificateNotFoundException;
 
+	/**
+	 * @param name is for searching all GiftCertificateAndItsTags with giftCertificate which contains part of this in
+	 *             its name
+	 * @return list of GiftCertificateAndItsTags which stored in the database with GiftCertificate which contains the
+	 * name with this part of the name
+	 * @throws DataInputException               when something goes wrong with data in the database or name is null
+	 * @throws TagNotFoundException             when something goes wrong with data in the database
+	 * @throws GiftCertificateNotFoundException when something goes wrong with data in the database
+	 */
 	List<GiftCertificateAndItsTags> findByPartOfName(String name)
 			throws DataInputException, TagNotFoundException, GiftCertificateNotFoundException;
 
+	/**
+	 * @param description is for searching all GiftCertificateAndItsTags with giftCertificate which contains part of
+	 *                    this in its description
+	 * @return list of GiftCertificateAndItsTags which stored in the database with GiftCertificate which contains the
+	 * name with this part of the description
+	 * @throws DataInputException               when something goes wrong with data in the database or description is null
+	 * @throws TagNotFoundException             when something goes wrong with data in the database
+	 * @throws GiftCertificateNotFoundException when something goes wrong with data in the database
+	 */
 	List<GiftCertificateAndItsTags> findByPartOfDescription(String description)
 			throws DataInputException, TagNotFoundException, GiftCertificateNotFoundException;
 
+	/**
+	 * @param giftCertificateAndItsTags is to be updated in the database with updating giftCertificate and tags
+	 * @throws GiftCertificateNotFoundException    when something goes wrong with data in the database
+	 * @throws DataInputException                  when something goes wrong with data in the database or giftCertificateAndItsTag is
+	 *                                             null
+	 * @throws TagNotFoundException                when something goes wrong with data in the database
+	 * @throws GiftCertificateTagNotFoundException when something goes wrong with data in the database
+	 */
 	void update(GiftCertificateAndItsTags giftCertificateAndItsTags) throws GiftCertificateNotFoundException,
 			DataInputException, TagNotFoundException, GiftCertificateTagNotFoundException;
 
+	/**
+	 * @param giftCertificateAndItsTags is for to be deleted from the database. It deletes only giftCertificate and
+	 *                                  giftCertificateTag. Tags are not to be deleted from the database
+	 * @throws GiftCertificateNotFoundException when something goes wrong with data in the database
+	 * @throws DataInputException               when something goes wrong with data in the database or giftCertificateAndItsTags is
+	 *                                          null
+	 */
 	void delete(GiftCertificateAndItsTags giftCertificateAndItsTags) throws GiftCertificateNotFoundException,
 			DataInputException;
 
+	/**
+	 * @param id is an id of giftCertificate to be deleted from the database. It deletes only giftCertificate and
+	 *           giftCertificateTag. Tags are not to be deleted from the database
+	 * @throws GiftCertificateNotFoundException when something goes wrong with data in the database
+	 * @throws DataInputException               when something goes wrong with data in the database or id is null
+	 */
 	void deleteById(Long id) throws GiftCertificateNotFoundException, DataInputException;
 
 }
--- service/src/main/java/com/itechart/esm/service/GiftCertificateTagService.java
+++ service/src/main/java/com/itechart/esm/service/GiftCertificateTagService.java
@@ -12,25 +12,79 @@ import java.util.List;
 
 public interface GiftCertificateTagService {
 
+	/**
+	 * @param giftCertificateTag is to be saved in the database
+	 * @throws DataInputException when giftCertificateTag is null
+	 */
 	void save(GiftCertificateTag giftCertificateTag) throws DataInputException;
 
+	/**
+	 * @return list of giftCertificateTags stored in the database
+	 * @throws GiftCertificateTagNotFoundException if there are no giftCertificateTags in the database
+	 * @throws DataInputException                  when something goes wrong in the database
+	 * @throws GiftCertificateNotFoundException    when data stored in the database is wrong
+	 * @throws TagNotFoundException                when data stored in the database is wrong
+	 */
 	List<GiftCertificateTag> findAll() throws GiftCertificateTagNotFoundException, DataInputException,
 			GiftCertificateNotFoundException, TagNotFoundException;
 
+	/**
+	 * @param id is for searching giftCertificateTag in the database
+	 * @return giftCertificateTag with this id
+	 * @throws GiftCertificateTagNotFoundException when there is no giftCertificateTag with this id
+	 * @throws DataInputException                  when something goes wrong in the database
+	 * @throws GiftCertificateNotFoundException    when data stored in the database is wrong
+	 * @throws TagNotFoundException                when data stored in the database is wrong
+	 */
 	GiftCertificateTag findById(Long id) throws GiftCertificateTagNotFoundException, DataInputException,
 			GiftCertificateNotFoundException, TagNotFoundException;
 
+	/**
+	 * @param tag is for searching giftCertificateTags in the database
+	 * @return list of giftCertificateTags with this tag
+	 * @throws DataInputException               when something goes wrong in the database
+	 * @throws GiftCertificateNotFoundException when data stored in the database is wrong
+	 * @throws TagNotFoundException             when data stored in the database is wrong
+	 */
 	List<GiftCertificateTag> findByTag(Tag tag) throws DataInputException, GiftCertificateNotFoundException,
 			TagNotFoundException;
 
+	/**
+	 * @param giftCertificate is for searching giftCertificateTags in the database
+	 * @return list of giftCertificateTags with this giftCertificate
+	 * @throws DataInputException               when something goes wrong in the database
+	 * @throws GiftCertificateNotFoundException when data stored in the database is wrong
+	 * @throws TagNotFoundException             when data stored in the database is wrong
+	 */
 	List<GiftCertificateTag> findByGiftCertificate(GiftCertificate giftCertificate) throws DataInputException,
 			GiftCertificateNotFoundException, TagNotFoundException;
 
+	/**
+	 * @param tagId             is for searching giftCertificateTags in the database
+	 * @param giftCertificateId is for searching giftCertificateTags in the database
+	 * @return list of giftCertificateTags with these tag and giftCertificate
+	 * @throws GiftCertificateTagNotFoundException when there is no giftCertificateTag with these tagId and
+	 *                                             giftCertificateId
+	 * @throws GiftCertificateNotFoundException    when data stored in the database is wrong
+	 * @throws TagNotFoundException                when data stored in the database is wrong
+	 * @throws DataInputException                  when something goes wrong in the database
+	 */
 	GiftCertificateTag findByTagIdAndGiftCertificateId(Long tagId, Long giftCertificateId)
 			throws GiftCertificateTagNotFoundException, GiftCertificateNotFoundException, TagNotFoundException, DataInputException;
 
+	/**
+	 * @param giftCertificateTag is to be deleted from the database
+	 * @throws GiftCertificateTagNotFoundException when there is no such giftCertificateTag stored in the database
+	 * @throws DataInputException                  when giftCertificateTag is null
+	 */
 	void delete(GiftCertificateTag giftCertificateTag) throws GiftCertificateTagNotFoundException, DataInputException;
 
+	/**
+	 * @param id is an id of giftCertificateTag which is to be deleted from the database
+	 * @throws GiftCertificateTagNotFoundException when there is no such giftCertificateTag with this id stored in the
+	 *                                             database
+	 * @throws DataInputException                  when id is null
+	 */
 	void deleteById(Long id) throws GiftCertificateTagNotFoundException, DataInputException;
 
 }
